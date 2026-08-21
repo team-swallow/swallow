c9de07b77c304fba8ba8c8df31425ec83eabe225
jambottle <jaewon.walks@gmail.com>
2026-08-22 00:05:10 +0900

    ESLint 설정 추가
    - import 구문 자동 정렬 (eslint-plugin-simple-import-sort)
    - import 구문 자동 제거 (eslint-plugin-unused-imports)
    - 의존성 배열 자동 정렬 (eslint-plugin-sort-react-dependency-arrays)

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,12 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import onlyWarn from "eslint-plugin-only-warn"
-import turboPlugin from "eslint-plugin-turbo"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import sortReactDependencyArrays from "eslint-plugin-sort-react-dependency-arrays";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +20,16 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+      "sort-react-dependency-arrays": sortReactDependencyArrays,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
+      "unused-imports/no-unused-imports": "warn",
+      "sort-react-dependency-arrays/sort": "warn",
     },
   },
   {
@@ -29,4 +40,4 @@ export const config = [
   {
     ignores: ["dist/**", ".next/**", "**/.turbo/**", "**/coverage/**"],
   },
-]
+];
